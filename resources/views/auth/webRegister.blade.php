bbb22ee51a031b30f2bf9c941b116215d5a9fd5a
sabreenMohammed107 <sabreenm312@gmail.com>
2023-05-30 01:05:45 +0200

    test

--- resources/views/auth/webRegister.blade.php
+++ resources/views/auth/webRegister.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="title" content="Champion's Academy" />
+    <meta name="title" content="Space Academy" />
     <link href="{{ asset('img/logo.png') }}" rel="shortcut icon">
     <!-- fontawesome  -->
     <link rel="stylesheet" href="{{ asset('web-assets/loginandout/all.min.css') }}">
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href="{{ asset('web-assets/loginandout/sign-in.css') }}">
     <!-- icon -->
     <link rel="icon" href="{{ asset('web-assets/ico/favicon.png') }}">
-    <title> Sign up </title>
+    <title>HTI Space </title>
 </head>
 
 <body>
